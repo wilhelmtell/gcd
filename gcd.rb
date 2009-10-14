ff00a57ac63c90bd237318dfa5547d84df812ab2
Matan Nassau <matan.nassau@gmail.com>
2009-10-14 15:02:38 -0400

    added extended option to find a^-1 mod b

--- gcd.rb
+++ gcd.rb
@@ -2,19 +2,35 @@
 
 require 'options'
 
-if __FILE__ == $0
+# returns [i,j,gcd] s.t. i*n+j*m=1
+def extended_euclidean(n,m,opts)
+  if m == 0
+    return 1,0,n
+  end
+  if opts.trace
+    f = n / m
+    puts "#{n}=#{f}*#{m}+#{n%m}"
+  end
+  i,j,gcd = extended_euclidean(m,n%m,opts)
+  i,j,gcd = j, i-((n/m)*j), gcd
+  puts "#{gcd}=#{i}*#{n}+#{j}*#{m}" if opts.trace and opts.extended
+  return [i,j,gcd]
+end
+
+# main entry
+def go_beaver_go()
   options = parse_commandline_options
   a = options.a || gets.to_i
   b = options.b || gets.to_i
-
-  while a > 0
-    r = b % a;
-    if options.trace
-      f = b / a
-      puts "#{b}=#{f}*#{a}+#{r}"
-    end
-    b = a;
-    a = r;
+  inv,k,gcd = extended_euclidean(a,b,options)
+  if inv < 0 and options.extended and options.trace
+    puts "#{inv}mod#{b}=#{inv%b}"
   end
-  puts b
+  print gcd
+  print ' ', inv % b if options.extended
+  puts
+end
+
+if __FILE__ == $0
+  go_beaver_go
 end
--- options.rb
+++ options.rb
@@ -4,10 +4,14 @@ require 'ostruct'
 def parse_commandline_options()
   options = OpenStruct.new
   options.trace = false
+  options.extended = false
   OptionParser.new do |opts|
     opts.banner = "Usage:  #{$0} [options]"
     opts.on("-t", "--trace", "Trace out the solution") do |v|
       options.trace = v
+    end
+    opts.on("-e", "--extended", "Find a^-1 mod b") do |v|
+      options.extended = v
     end
     opts.on("-a", "--a INTEGER", Integer, "Set value for a") do |v|
       options.a = v
