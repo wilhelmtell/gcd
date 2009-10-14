79694b09f18c2cc283c30408acceb47587d90be5
Matan Nassau <matan.nassau@gmail.com>
2009-10-14 13:13:47 -0400

    split option parsing into options.rb

--- gcd.rb
+++ gcd.rb
@@ -1,25 +1,6 @@
 #!/usr/bin/env ruby
 
-require 'optparse'
-require 'ostruct'
-
-def parse_commandline_options()
-  options = OpenStruct.new
-  options.trace = false
-  OptionParser.new do |opts|
-    opts.banner = "Usage:  #{$0} [options]"
-    opts.on("-t", "--trace", "Trace out the solution") do |v|
-      options.trace = v
-    end
-    opts.on("-a", "--a INTEGER", Integer, "Set value for a") do |v|
-      options.a = v
-    end
-    opts.on("-b", "--b INTEGER", Integer, "Set value for b") do |v|
-      options.b = v
-    end
-  end.parse!
-  return options
-end
+require 'options'
 
 if __FILE__ == $0
   options = parse_commandline_options
---
+++ options.rb
@@ -0,0 +1,21 @@
+require 'optparse'
+require 'ostruct'
+
+def parse_commandline_options()
+  options = OpenStruct.new
+  options.trace = false
+  OptionParser.new do |opts|
+    opts.banner = "Usage:  #{$0} [options]"
+    opts.on("-t", "--trace", "Trace out the solution") do |v|
+      options.trace = v
+    end
+    opts.on("-a", "--a INTEGER", Integer, "Set value for a") do |v|
+      options.a = v
+    end
+    opts.on("-b", "--b INTEGER", Integer, "Set value for b") do |v|
+      options.b = v
+    end
+  end.parse!
+  return options
+end
+
